eff22aef527901bd087ffe99358153fb1ceab0f2
Jenifer Rajendren <43472476+Jenifer-TheCoder@users.noreply.github.com>
2024-02-12 14:09:45 +0530

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,55 @@
+use reqwest;
+use serde::Deserialize;
+use tokio;
+
+#[derive(Deserialize)]
+struct WeatherData {
+    main : Main,
+    weather : Vec<Weather>,
+}
+#[derive(Deserialize)]
+struct Main {
+    temp : f64,
+}
+#[derive(Deserialize)]
+struct Weather {
+    description : String,
+}
+
+async fn fetch_weather(api_key : &str , city : String) -> Result<(), reqwest::Error>{
+
+    let url = format!("https://api.openweathermap.org/data/2.5/weather?q={}&appid={}", city.trim(),
+                                api_key);
+
+    let response = reqwest::get(url).await?;
+
+    if response.status().is_success() {
+
+        let weather_data : WeatherData = response.json().await?;
+
+        let temperature = weather_data.main.temp;
+        let temperature_celsius =temperature - 273.15;
+
+        let description = &weather_data.weather[0].description;
+        println!("Weather in city: {}, {:.2}°C , {}" , city.trim(), temperature_celsius, description);
+
+    }
+    else {
+        println!("Error : {}", response.status());
+    }
+    Ok(())
+
+}
+#[tokio::main]
+async fn main() {
+    let api_key = "e5c9e66275afa5767ef301b14caaca58";
+    let mut city = String::new();
+    println!("Enter the city");
+
+    std::io::stdin().read_line(&mut city).expect("Failed to read the line");
+
+    tokio::spawn(fetch_weather(api_key, city));
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+ 
+}
